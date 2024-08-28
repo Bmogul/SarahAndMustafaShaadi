7d51822c82746a1f6d7f4c9348955011deb5e324
Snow <b.mogul02@gmail.com>
2024-08-28 18:34:54 -0400

    removed metadata image

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,13 +14,6 @@ export const metadata = {
     title: "ShaadiVite",
     description: "Important Information Inside",
     url: "https://shaadi1446.vercel.app", // Replace with your website URL
-    images: [
-      {
-        url: "https://i.imgur.com/iRPQK7P.png", // Replace with your image URL
-        width: 250, // Adjust the width and height as per your image dimensions
-        height: 200,
-      },
-    ],
   },
 };
 export default function RootLayout({ children }) {
